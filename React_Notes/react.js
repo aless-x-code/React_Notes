2124db57d8cddbd739f1997bb2dc7bb8465667ba
Alessandro Campigotto <alessandrocampigotto@MacBook-Pro.lan>
2022-08-30 20:14:40 -0400

    updated 8/30/22

--- React_Notes/react.js
+++ React_Notes/react.js
@@ -982,3 +982,336 @@ class Colorful extends React.Component {
     return <div style={styles}>Style Me!</div>;
   }
 }
+
+// ________________________________________________________________________
+// print random array element
+
+const inputStyle = {
+  width: 235,
+  margin: 5,
+};
+
+class MagicEightBall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      randomIndex: "",
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask() {
+    if (this.state.userInput) {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: "",
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value,
+    });
+  }
+  render() {
+    const possibleAnswers = [
+      "It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes, definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Most likely",
+      "Outlook not so good",
+      "Very doubtful",
+    ];
+    const answer = possibleAnswers[this.state.randomIndex]; // Change this line
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleChange}
+          style={inputStyle}
+        />
+        <br />
+        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+        <br />
+        <h3>Answer:</h3>
+        <p>
+          {/* Change code below this line */}
+          {answer}
+          {/* Change code above this line */}
+        </p>
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+// if/else statements
+
+class MyComponent4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+    if (this.state.display == true) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+  }
+}
+
+// ________________________________________________________________________
+// if else with && for more conscise code
+
+class MyComponent3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+        {/* if/else statement: => in the render method insert => {conditon && html}, if condition is true, html will print, otherwise will fail and return nothing*/}
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+// ternary operator
+
+const inputStyle3 = {
+  width: 235,
+  margin: 5,
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      userAge: "",
+    };
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: "",
+    });
+  }
+  submit() {
+    this.setState((state) => ({
+      userAge: state.input,
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        <div>
+          {this.state.userAge == "" // if age is empty = submit button
+            ? buttonOne
+            : this.state.userAge >= 18 // if not empty, but above or equal to 18 = enter button
+            ? buttonTwo
+            : this.state.userAge < 18 // if not empty, but below 18 = no pass button
+            ? buttonThree
+            : undefined}
+        </div>
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+// GameOfChance
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>{this.props.fiftyFifty >= 0.5 ? "You Win!" : "You Lose!"}</h1>;
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => {
+      return {
+        counter: this.state.counter + 1,
+      };
+    });
+  }
+  render() {
+    const expression = Math.random().toFixed(2);
+
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        <Results fiftyFifty={expression} />
+        <p>{"Turn: " + this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+// conditions and css inlines
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+  render() {
+    let inputStyle = {
+      border: "1px solid black",
+    };
+
+    let characters = this.state.input.length;
+
+    if (characters > 15) {
+      inputStyle.border = "3px solid red";
+    }
+
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+// map array and return
+
+const textAreaStyles = {
+  width: 235,
+  margin: 5,
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      userInput: "",
+      toDoList: [],
+    };
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(",");
+    this.setState({
+      toDoList: itemsArray,
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value,
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map((item) => <li>{item}</li>); // Change this line
+    // eat, code, sleep, repeat
+
+    console.log(items);
+
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder="Separate Items With Commas"
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+// ________________________________________________________________________
+//
